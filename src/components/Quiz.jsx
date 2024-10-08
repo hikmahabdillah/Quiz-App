435bb5b0a9d4faeefea644f051e4fc20c5636322
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-10-08 23:50:21 +0700

    display question & answer with the event related to it, add event for reset local storage if user has finished or times is up, and add favicon

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -3,6 +3,7 @@ import CurrentQuest from "./molecules/CurrentQuest";
 import Question from "./molecules/Question";
 import Timer from "./molecules/Timer";
 import { getQuestions } from "../hooks/useQuestion";
+// import { useNavigate, Navigate } from "react-router-dom";
 
 const QuizForm = () => {
   const listQuestions = getQuestions();
@@ -36,16 +37,48 @@ const QuizForm = () => {
     }
   }, [questions, currentQ, answers, timeLeft]);
 
+  // jika waktu habis akan reset quiz(sementara)
+  useEffect(() => {
+    if (timeLeft === 0) {
+      clearLocalStorage()
+    };
+  }, [timeLeft]);
+
+  // ketika user memilih jawaban
+  const handleAnswer = (answer) => {
+    // menyimpan jawaban yang dipilih
+    setAnswers([...answers, answer]);
+
+    // move to next quest
+    if (currentQ < questions.length - 1) {
+      setCurrentQ(currentQ + 1);
+    } else {
+      clearLocalStorage();
+    }
+  };
+
+  const clearLocalStorage = () => {
+    localStorage.removeItem("quizData");
+    setQuestions([]); // Reset questions setelah menghapus
+    setCurrentQ(0);
+    setAnswers([]);
+    setTimeLeft(1200); 
+  };
+
   return (
     <>
       {startQuiz ? (
-        <div className="w-full p-5 rounded-lg shadow border max-w-md sm:max-w-lg bg-gray-800 border-gray-700">
-          <div className="flex items-center justify-between">
-            <CurrentQuest currentQ={currentQ} totalQ={questions.length} />
-            <Timer timeLeft={timeLeft} setTimeLeft={setTimeLeft} />
+        questions.length > 0 ? (
+          <div className="w-full p-5 rounded-lg shadow border max-w-md sm:max-w-lg bg-gray-800 border-gray-700">
+            <div className="flex items-center justify-between">
+              <CurrentQuest currentQ={currentQ} totalQ={questions.length} />
+              <Timer timeLeft={timeLeft} setTimeLeft={setTimeLeft} />
+            </div>
+            <Question data={questions[currentQ]} handleAnswer={handleAnswer} />
           </div>
-          <Question />
-        </div>
+        ) : (
+          <p className="text-slate-50 text-center font-semibold">Loading...</p>
+        )
       ) : (
         <button
           type="button"
--- src/components/molecules/Question.jsx
+++ src/components/molecules/Question.jsx
@@ -1,27 +1,104 @@
-const Question = () => {
+import { useState, useEffect } from "react";
+
+const Question = ({ data, handleAnswer }) => {
+  // Check ketersediaan data
+  if (!data) {
+    return (
+      <div className="my-5 text-slate-50 text-center font-semibold">
+        Loading...
+      </div>
+    );
+  }
+
+  // untuk mengubah format soal agar mudah terbaca(menghilangkan kode html entity)
+  const decodeHTML = (str) => {
+    const parser = new DOMParser().parseFromString(str, "text/html");
+    return parser.documentElement.textContent;
+  };
+  const decodeQuestion = decodeHTML(data.question);
+
+  const [answers, setAnswers] = useState([]);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [isAnswerSelected, setIsAnswerSelected] = useState(false);
+
+  // untuk menggabungkan semua jawaban dan mengacak jawaban tersebut
+  useEffect(() => {
+    const allAnswers = [data.correct_answer, ...data.incorrect_answers];
+    const shuffleAnswers = allAnswers.sort(() => Math.random() - 0.5);
+    setAnswers(shuffleAnswers);
+
+    // Reset jawaban yang dipilih saat soal berubah
+    setSelectedAnswer(null);
+    setIsAnswerSelected(false);
+  }, [data]);
+
+  const handleSelectAnswer = (answer) => {
+    setSelectedAnswer(answer);
+    setIsAnswerSelected(true);
+
+    setTimeout(() => {
+      handleAnswer(answer);
+    }, 1500);
+  };
+
+  const handleCorrectAnswer =
+    selectedAnswer === data.correct_answer ? "bg-green-500" : "bg-red-400";
+
   return (
     <div className="mt-5 w-full p-3 sm:p-5 rounded-lg shadow border max-w-md sm:max-w-lg bg-gray-700 border-gray-800">
       <h1 className="mb-5 font-semibold text-lg sm:text-xl text-slate-50">
-        Apakah saya akan menjadi seorang Front End Developer di masa depan?
+        {data && decodeQuestion}
       </h1>
       <div className="w-full flex flex-col gap-3 mb-3">
-        <div className="w-full flex items-center ps-4 bg-gray-800 border rounded-lg border-gray-800">
-          <input
-            id="bordered-radio-1"
-            type="radio"
-            value=""
-            name="bordered-radio"
-            className="size-5 text-blue-600focus:ring-blue-600 ring-offset-gray-800 focus:ring-2 bg-gray-700 border-gray-600"
+        {answers.map((answer, index) => (
+          <InputChoice
+            key={index}
+            order={index}
+            answer={answer}
+            handleCorrectAnswer={handleCorrectAnswer}
+            handleSelectAnswer={handleSelectAnswer}
+            selectedAnswer={selectedAnswer}
+            isAnswerSelected={isAnswerSelected}
           />
-          <label
-            htmlFor="bordered-radio-1"
-            className="w-full py-4 ms-2 text-sm sm:text-base font-medium text-gray-300"
-          >
-            Iya
-          </label>
-        </div>
+        ))}
       </div>
     </div>
+  );
+};
+
+const InputChoice = ({
+  order,
+  answer,
+  handleSelectAnswer,
+  selectedAnswer,
+  handleCorrectAnswer,
+  isAnswerSelected,
+}) => {
+  return (
+    <>
+      <div
+        className={`w-full flex items-center ps-4 ${
+          (selectedAnswer === answer && handleCorrectAnswer) || "bg-gray-800"
+        } border rounded-lg border-gray-800 duration-300`}
+      >
+        <input
+          id={order}
+          type="radio"
+          onChange={() => handleSelectAnswer(answer)}
+          value={answer}
+          name="bordered-radio"
+          className="size-5 text-blue-600focus:ring-blue-600 ring-offset-gray-800 focus:ring-2 bg-gray-700 border-gray-600"
+          checked={selectedAnswer === answer}
+          disabled={isAnswerSelected}
+        />
+        <label
+          htmlFor={order}
+          className="w-full py-4 ms-2 text-sm sm:text-base font-medium text-gray-300"
+        >
+          {answer}
+        </label>
+      </div>
+    </>
   );
 };
 
