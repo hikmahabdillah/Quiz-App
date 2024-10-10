8a6b6a10e15aa0b561457ccffb643631030910e1
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-10-10 22:25:31 +0700

    add function to count the result

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,23 +1,70 @@
 import ResultCard from "./molecules/ResultCard";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Result = () => {
   const navigate = useNavigate();
+  const [correctAnswers, setCorrectAnswers] = useState([]);
+  const [totalTrue, setTotalTrue] = useState();
+  const [totalFalse, setTotalFalse] = useState();
+  const [totalAnswers, setTotalAnswers] = useState();
+  const [score, setScore] = useState();
 
   const clearLocalStorage = () => {
     localStorage.removeItem("quizData");
   };
 
+  useEffect(() => {
+    const savedQuiz = JSON.parse(localStorage.getItem("quizData"));
+    if (savedQuiz) {
+      // hitung jumlah jawaban
+      const answers = savedQuiz.answers;
+      setTotalAnswers(answers.length);
+
+      // get correctAnswer dari semua soal
+      const correctAnswer = savedQuiz.questions.map((item) => {
+        return item.correct_answer;
+      });
+      setCorrectAnswers(correctAnswer);
+
+      // menghitung jumlah benar
+      const totalTrueAnswer = answers.filter((item) =>
+        correctAnswers.includes(item)
+      );
+      setTotalTrue(totalTrueAnswer.length);
+
+      // menghitung total salah
+      let totalFalseAnswer = answers.length - totalTrueAnswer.length;
+      setTotalFalse(totalFalseAnswer);
+
+      // hitung skor
+      let score = totalTrue * 5;
+      setScore(score);
+    }
+  }, [correctAnswers, totalTrue]);
+
   return (
     <div className="w-full p-5 rounded-lg shadow border max-w-md sm:max-w-lg bg-gray-800 border-gray-700">
-      <h1 className="my-8 font-semibold text-center text-3xl text-slate-50">
-        Score: <span className="text-yellow-500">90%</span>
-      </h1>
-      <div className="flex mb-3 justify-center gap-2 flex-wrap sm:flex-nowrap">
-        <ResultCard icon={"../../check.svg"} total={18} typeContent="True" />
-        <ResultCard icon={"../../close.svg"} total={2} typeContent="False" />
-        <ResultCard icon={""} total={20} typeContent="Answers" />
-      </div>
+      {correctAnswers && (
+        <>
+          <h1 className="my-8 font-semibold text-center text-3xl text-slate-50">
+            Score: <span className="text-yellow-500">{score} %</span>
+          </h1>
+          <div className="flex mb-3 justify-center gap-2 flex-wrap sm:flex-nowrap">
+            <ResultCard
+              icon={"../../check.svg"}
+              total={totalTrue}
+              typeContent="True"
+            />
+            <ResultCard
+              icon={"../../close.svg"}
+              total={totalFalse}
+              typeContent="False"
+            />
+            <ResultCard icon={""} total={totalAnswers} typeContent="Answers" />
+          </div>
+        </>
+      )}
       <button
         type="button"
         onClick={() => {
